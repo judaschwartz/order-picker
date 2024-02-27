4b46d4ec0a4dd27bc9b34ac05ed40b97c51f5a56
Yehuda Schwartz <yehuda.schwartz@hcl.com>
2024-02-27 09:42:40 -0500

    add sample and skip missing order

--- processDiffs.js
+++ processDiffs.js
@@ -4,20 +4,24 @@ const resultsByUser = ['Order #, Picker, Comments, Missing items']
 const resultsByItm = {}
 let skipped = 0
 orderIds.forEach(id => {
-  const order = fs.readFileSync(`orders/${id}.csv`).toString().split("\n").map(l => l.split(','))
-  const diff = [id, order[1][0], order[2][0]]
-  if (order[2].length < 2) {
-    skipped++
-    diff.push('ORDER WAS NOT PICKED')
+  const diff = [id]
+  if (fs.existsSync(`orders/${id}.csv`)) {
+    const order = fs.readFileSync(`orders/${id}.csv`).toString().split("\n").map(l => l.split(','))
+    if (order[2].length < 2) {
+      skipped++
+      diff.push(order[1][0], order[2][0], 'ORDER WAS NOT PICKED')
+    } else {
+      order[0].slice(1).forEach((name, i) => {
+        const ordered = Number(order[1][i + 1]) || 0
+        const picked = Number(order[2][i + 1]) || 0
+        if (ordered != picked) {
+          diff.push(order[1][0], order[2][0], `${ordered - picked} "${name}"`)
+          resultsByItm[name] = resultsByItm[name] ? [...resultsByItm[name], [`${ordered - picked} ${id}`]] : [[`${ordered - picked} ${id}`]]
+        }
+      })
+    }
   } else {
-    order[0].slice(1).forEach((name, i) => {
-      const ordered = Number(order[1][i + 1]) || 0
-      const picked = Number(order[2][i + 1]) || 0
-      if (ordered != picked) {
-        diff.push(`${ordered - picked} "${name}"`)
-        resultsByItm[name] = resultsByItm[name] ? [...resultsByItm[name], [`${ordered - picked} ${id}`]] : [[`${ordered - picked} ${id}`]]
-      }
-    })
+    diff.push('', '', 'Order record does not exist')
   }
   resultsByUser.push(diff.join(','))
 })
