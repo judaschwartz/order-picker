737b0ec270088b932dca841a9f01e32d682805b8
Yehuda Schwartz <yehuda.schwartz@hcl.com>
2024-02-27 14:46:07 -0500

    tweeks

--- makeOrders.js
+++ makeOrders.js
@@ -1,12 +1,11 @@
 const fs = require('fs')
 const orders = fs.readFileSync('orders/allOrders.csv').toString().split("\n")
-
 const jsonFile = {}
 const itms = orders[0].trim().split(',').slice(2)
 orders.slice(1).forEach(l => {
   l = l.trim().split(',')
   jsonFile[l[0]] = l[1].toLocaleUpperCase()
-  const ord = ['name,order,picked', `${l[1]},,`, ...itms.map((n, i) => `${n},${l[i + 2]},`)]
+  const ord = ['name,ordered,picked', `${l[1]},,`, ...itms.map((n, i) => `${n},${l[i + 2]},`)]
   fs.writeFileSync(`orders/${l[0]}.csv`, ord.join("\n"))
 })
 fs.writeFileSync('orders.json', JSON.stringify(jsonFile))
